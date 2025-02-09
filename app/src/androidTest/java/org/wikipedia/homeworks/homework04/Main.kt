ffc6a3a9c87d9f0b922f4c31da564da5e28a29d2
Hexenhammer01 <railxpp@gmail.com>
2025-02-09 22:26:26 +0300

    homework04

--- app/src/androidTest/java/org/wikipedia/homeworks/homework04/Main.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework04/Main.kt
@@ -10,44 +10,44 @@ fun main() {
     schedule.addSchedule(Days.MONDAY, ScheduleEntity("Biology", LocalTime.of(10, 30), LocalTime.of(11, 10)))
     schedule.addSchedule(Days.MONDAY, ScheduleEntity("Chemistry", LocalTime.of(11, 15), LocalTime.of(11, 55)))
 
-// Так добавляется расписание с использованием DSL
-//    schedule {
-//
-//        monday {
-//            "10:30".."11:10" schedule "Biology"
-//            "11:15".."11:55" schedule "Chemistry"
-//            "09:00".."09:40" schedule "Mathematics"
-//            "09:45".."10:25" schedule "History"
-//        }
-//
-//        tuesday {
-//            "09:00".."09:40" schedule "English"
-//            "09:45".."10:25" schedule "Geography"
-//            "11:15".."11:55" schedule "Art"
-//            "10:30".."11:10" schedule "Physics"
-//        }
-//
-//        wednesday {
-//            "11:15".."11:55" schedule "Biology"
-//            "09:00".."09:40" schedule "Literature"
-//            "10:30".."11:10" schedule "History"
-//            "09:45".."10:25" schedule "Mathematics"
-//        }
-//
-//        thursday {
-//            "11:15".."11:55" schedule "Physics"
-//            "10:30".."11:10" schedule "Geography"
-//            "09:00".."09:40" schedule "Chemistry"
-//            "09:45".."10:25" schedule "English"
-//        }
-//
-//        friday {
-//            "09:45".."10:25" schedule "Literature"
-//            "11:15".."11:55" schedule "History"
-//            "09:00".."09:40" schedule "Art"
-//            "10:30".."11:10" schedule "Mathematics"
-//        }
-//    }
+ //Так добавляется расписание с использованием DSL
+    schedule {
+
+        monday {
+            "10:30".."11:10" schedule "Biology"
+            "11:15".."11:55" schedule "Chemistry"
+            "09:00".."09:40" schedule "Mathematics"
+            "09:45".."10:25" schedule "History"
+        }
+
+        tuesday {
+            "09:00".."09:40" schedule "English"
+            "09:45".."10:25" schedule "Geography"
+            "11:15".."11:55" schedule "Art"
+            "10:30".."11:10" schedule "Physics"
+        }
+
+        wednesday {
+            "11:15".."11:55" schedule "Biology"
+            "09:00".."09:40" schedule "Literature"
+            "10:30".."11:10" schedule "History"
+            "09:45".."10:25" schedule "Mathematics"
+        }
+
+        thursday {
+            "11:15".."11:55" schedule "Physics"
+            "10:30".."11:10" schedule "Geography"
+            "09:00".."09:40" schedule "Chemistry"
+            "09:45".."10:25" schedule "English"
+        }
+
+        friday {
+            "09:45".."10:25" schedule "Literature"
+            "11:15".."11:55" schedule "History"
+            "09:00".."09:40" schedule "Art"
+            "10:30".."11:10" schedule "Mathematics"
+        }
+    }
 
     println(schedule.toString())
 }
--- app/src/androidTest/java/org/wikipedia/homeworks/homework04/Shedule.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework04/Shedule.kt
@@ -8,11 +8,54 @@ data class ScheduleEntity(val lesson: String, val startTime: LocalTime, val endT
 
 class Schedule {
 
+
     private val scheduleOfWeek = mutableMapOf<Days, MutableList<ScheduleEntity>>()
     private val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
 
+
     fun addSchedule(day: Days, scheduleEntity: ScheduleEntity) {
         scheduleOfWeek.getOrPut(day) { mutableListOf() }.add(scheduleEntity)
+    }
+
+    private var day: Days? = null
+
+    private fun scheduleDay(day: Days, function: () -> Unit) {
+        this.day = day
+        function()
+        this.day = null
+    }
+
+
+    operator fun invoke (function: Schedule.() -> Unit){
+        function(this)
+    }
+
+    fun monday(function: () -> Unit){
+        scheduleDay(Days.MONDAY,function)
+    }
+
+    fun tuesday(function: () -> Unit){
+        scheduleDay(Days.TUESDAY,function)
+    }
+
+    fun wednesday(function: () -> Unit){
+        scheduleDay(Days.WEDNESDAY,function)
+    }
+
+    fun thursday(function: () -> Unit){
+        scheduleDay(Days.THURSDAY,function)
+    }
+
+    fun friday(function: () -> Unit){
+        scheduleDay(Days.FRIDAY,function)
+    }
+
+    fun saturday(function: () -> Unit){
+        scheduleDay(Days.SATURDAY,function)
+    }
+
+    fun sunday(function: () -> Unit){
+        scheduleDay(Days.SUNDAY,function)
     }
 
     override fun toString(): String {
