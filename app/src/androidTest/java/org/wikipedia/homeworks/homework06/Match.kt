5b0b10e98379f047defaef33fb490dd2d96df745
Hexenhammer01 <railxpp@gmail.com>
2025-02-17 00:25:54 +0300

    homework06

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework06/Match.kt
@@ -0,0 +1,18 @@
+package org.wikipedia.homeworks.homework06
+
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
+
+enum class Colors { RED, BLUE, GREEN, YELLOW, BLACK, WHITE }
+
+data class Shape(val sideLength: Float, val sides: Int, val color: Color)
+
+class ColorM(private val expectedColors: Colors) : TypeSafeMatcher<Shape>() {
+    override fun describeTo(description: Description) {
+        description.appendText("имеет цвет  $expectedColors")
+    }
+    override fun matchesSafely(item: Shape): Boolean {
+        return item.color == expectedColors
+    }
+}
+fun hasColor(expectedColors: Colors) = ColorM(expectedColors)
