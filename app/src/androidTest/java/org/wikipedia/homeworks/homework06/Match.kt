2983421e55a5afa7e5ed2ae0d2ebd3663ad5adc4
Hexenhammer01 <railxpp@gmail.com>
2025-02-17 11:11:24 +0300

    homework06

--- app/src/androidTest/java/org/wikipedia/homeworks/homework06/Match.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework06/Match.kt
@@ -52,19 +52,14 @@ fun isOtrizatelnayaStoronaMatcher() = OtrizatelnayaStoronaMatcher()
 
 enum class Color { RED, BLUE, GREEN, YELLOW, BLACK, WHITE }
 
-val shapes = listOf(
-    Shape(10f, 3, Color.RED), Shape(5f, 4, Color.BLUE), Shape(7f, 2, Color.GREEN),
-    Shape(0.5f, 1, Color.YELLOW), Shape(-3f, 5, Color.BLACK), Shape(8f, -2, Color.WHITE),
-    Shape(12f, 6, Color.RED), Shape(15f, 8, Color.BLUE), Shape(20f, 4, Color.GREEN),
-    Shape(9f, 5, Color.YELLOW), Shape(2f, 3, Color.BLACK), Shape(11f, 7, Color.WHITE),
-    Shape(6f, 10, Color.RED), Shape(3f, 2, Color.BLUE), Shape(4f, 1, Color.GREEN),
-    Shape(25f, 12, Color.YELLOW), Shape(30f, 14, Color.BLACK), Shape(35f, 16, Color.WHITE),
-    Shape(40f, 18, Color.RED), Shape(50f, 20, Color.BLUE)
-)
-val filter = shapes.filter { shape ->
-    allOf(
-        isDlinna(456f, -3453f),
-        isOtrizatelnoeKovichestvoStoronMatcher(),
-        isOtrizatelnayaStoronaMatcher()
-        ).matches(shape)
+fun main () {
+    val shapes = listOf(
+        Shape(10f, 3, Color.RED), Shape(5f, 4, Color.BLUE), Shape(7f, 2, Color.GREEN),
+        Shape(0.5f, 1, Color.YELLOW), Shape(-3f, 5, Color.BLACK), Shape(8f, -2, Color.WHITE),
+        Shape(12f, 6, Color.RED), Shape(15f, 8, Color.BLUE), Shape(20f, 4, Color.GREEN),
+        Shape(9f, 5, Color.YELLOW), Shape(2f, 3, Color.BLACK), Shape(11f, 7, Color.WHITE),
+        Shape(6f, 10, Color.RED), Shape(3f, 2, Color.BLUE), Shape(4f, 1, Color.GREEN),
+        Shape(25f, 12, Color.YELLOW), Shape(30f, 14, Color.BLACK), Shape(35f, 16, Color.WHITE),
+        Shape(40f, 18, Color.RED), Shape(50f, 20, Color.BLUE)
+    )
 }
