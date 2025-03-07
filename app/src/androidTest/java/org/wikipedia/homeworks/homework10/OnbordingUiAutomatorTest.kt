1d2f1935709cbc95fcf7d4fd27effad0c27fe78b
Hexenhammer01 <railxpp@gmail.com>
2025-03-07 17:21:38 +0300

    homework10

--- app/src/androidTest/java/org/wikipedia/homeworks/homework10/OnbordingUiAutomatorTest.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework10/OnbordingUiAutomatorTest.kt
@@ -1,15 +1,7 @@
 package org.wikipedia.homeworks.homework10
 
-import android.annotation.SuppressLint
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.swipeRight
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import com.kaspersky.components.kautomator.component.common.actions.UiSwipeableActions
-import com.kaspersky.components.kautomator.component.switch.UiSwitchableActions
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import okhttp3.internal.wait
 import org.junit.Rule
 import org.junit.Test
 import org.wikipedia.main.MainActivity
